079a46e9fb244ba3a153a387739f55710410d45a
Chris <cmcooling@gmail.com>
2019-10-18 11:43:25 +0100

    Arrays 2D

---
+++ Variables/arrays2d.f90
@@ -0,0 +1,16 @@
+program arrays2d
+
+    real, dimension(3,4)    :: array1
+
+    array1=4
+    array1(2,1)=1
+    array1(1,:)=(/5,10,4,6/)
+    array1(2:, 2:)=2
+
+    print*, "Array1 all: ", array1
+
+    print*, "Array1 row 1: ",array1(1,:)
+    print*, "Array1 row 2: ",array1(2,:)
+    print*, "Array1 row 3: ",array1(3,:)
+
+end program arrays2d
