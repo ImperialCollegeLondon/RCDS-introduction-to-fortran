33404a261b80c7ebb2735c46313c485234fb1a6c
Coolernato <cmcooling@gmail.com>
2019-10-28 14:27:44 +0000

    Loops examples

---
+++ Loops/do_loops.f90
@@ -0,0 +1,29 @@
+program do_loops
+
+    integer ::  counter
+
+    print*, "Loop 1"
+
+    do counter=1, 10, 2
+        print*, counter
+    end do
+
+    print*, "Loop 2"
+
+    do counter=5, 10
+        print*, counter
+    end do
+
+    print*, "Loop 3"
+
+    do counter=95, 90, -1
+        print*, counter
+    end do
+
+    print*, "Loop 4"
+
+    do counter=1, 10, -2
+        print*, counter
+    end do
+
+end program do_loops
---
+++ Loops/do_while_loops.f90
@@ -0,0 +1,34 @@
+program do_while_loops
+
+    real    ::  real1
+    integer ::  int1
+
+    real1=1.0
+
+    print*, "Loop 1"
+    do while (real1<10.0)
+        real1=real1*2.0
+        print*, real1
+    end do
+
+    print*, "Loop 2"
+    int1=2
+    do while (mod(int1, 5)/=0)
+        int1=int1+2
+        print*, int1
+    end do
+
+    print*, "Loop 3"
+    int1=0
+    real1=1.0
+    do while (real1<16)
+        int1=int1+1
+        if (int1>5)then
+            real1=real1*2
+        end if
+        print*, int1, real1
+    end do
+
+
+
+end program do_while_loops
