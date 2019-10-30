67a4f1ae70366ae8cb11a97f037efb774c3e8a0e
Chris <cmcooling@gmail.com>
2019-10-30 16:56:39 +0000

    Functions Initial

---
+++ Functions/functions.f90
@@ -0,0 +1,11 @@
+program functions
+
+    print*, function1()
+
+    contains
+
+        real function function1()
+            function1=1.0
+        end function function1
+
+end program functions
