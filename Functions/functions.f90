74023c84deb4a1025cac088124161fec4f2a216d
Chris <cmcooling@gmail.com>
2019-10-31 09:41:38 +0000

    Functions Update

--- Functions/functions.f90
+++ Functions/functions.f90
@@ -1,11 +1,29 @@
 program functions
 
-    print*, function1()
+    print*, function_name1(2.0, 4.0)
+    print*, function_name2(1.0, 3.0)
+    print*, function_name1(1.0, 3.0)
+    print*, function_name2(2.0, 4.0)
 
     contains
 
-        real function function1()
-            function1=1.0
-        end function function1
+    real function function_name1(arg1, arg2)
+	    real	::	arg1, arg2
+
+	    function_name=arg1*arg2
+
+    end function function_name1
+
+
+
+    function function_name2(arg1, arg2)result(evaluation)
+        real	::	arg1, arg2, evaluation
+    
+        evaluation=arg1*2.0
+    
+    end function function_name2
+    
+
+
 
 end program functions
