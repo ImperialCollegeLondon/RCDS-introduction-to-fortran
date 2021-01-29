520281fd9f9f1b39a5f4b9375c50635bcf789bbc
coolernato <cmcooling@gmail.com>
2021-01-29 11:06:32 +0000

    Replaced incorrect error file for exercise

--- Errors/compilation_errors_exercise.f90
+++ Errors/compilation_errors_exercise.f90
@@ -1,23 +1,20 @@
 program compilation_errors
-!This program is intended to take two numbers and adds them together using a function
-
-    implicit none
-
-    real ::  value1, value2 
-
-    value1=1
-    value2=2
-
-    print*, addition(value1, value2)
-
-    contains
-
-    function addition(a, b)result(evaluation)
-        real    ::  a, b, evaluation
-
-        evaluation=a+b
-
-    end function addition
-
-end program compilation_errors
-
+    !This program is intended to take two numbers and adds them together using a function
+    
+        implicit none
+    
+        rel ::  value1, value2 
+    
+        value1=1
+        value3=2
+    
+        print* addition(value1, value2)
+    
+    
+    
+        function addition(a, b)reslt(evaluation)
+            real    ::  a, evaluation
+    
+            evaluation==a+b
+    
+        end subroutine adition
---
+++ Errors/compilation_errors_exercise_correct.f90
@@ -0,0 +1,23 @@
+program compilation_errors
+!This program is intended to take two numbers and adds them together using a function
+
+    implicit none
+
+    real ::  value1, value2 
+
+    value1=1
+    value2=2
+
+    print*, addition(value1, value2)
+
+    contains
+
+    function addition(a, b)result(evaluation)
+        real    ::  a, b, evaluation
+
+        evaluation=a+b
+
+    end function addition
+
+end program compilation_errors
+
